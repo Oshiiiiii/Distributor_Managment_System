907aa39f081ddee7291499c36d57cc1fb4d3f8a6
Oshani Wathsala <oshani@gmail.com>
2025-03-10 01:13:16 +0530

    Initial commit

---
+++ src/test/java/com/fmcg/route_management/RouteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmcg.route_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
